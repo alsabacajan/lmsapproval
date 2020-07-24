848b0466c08c9f1ddb288d37d246ba493fb828ab
alsabacajan <61576408+alsabacajan@users.noreply.github.com>
2020-07-24 11:39:32 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config({silent:true});
+
+const express = require('express');
+
+const app = express();
+
+// Setup middleware for parsing application/json
+app.use(express.json());
+// Setup middleware for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const private = require('./routes/private');
+ 
+
+const mw = require('./middlewares/auth');
+ 
+//app.use('/private', mw.auth, private);  
+app.use('/private', private);
+app.listen(process.env.PORT, function(){
+  console.log('Listening @ http://localhost:' + process.env.PORT);
+});
